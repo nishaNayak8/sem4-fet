bcfe3a3f1be8fe6a89327524e99cfab83e3c6a7b
nishaNayak8 <4mt24cs115@mite.ac.in>
2026-05-01 18:06:09 +0530

    Update one_script.js

--- Assignments/one_script.js
+++ Assignments/one_script.js
@@ -10,11 +10,20 @@ courseLine.addEventListener("click", function () {
     if (newText !== null && newText.trim() !== "") {
         courseLine.textContent = newText;
 
-        // Step 2: change font size after text update
-        let size = prompt("Enter font size (e.g., 20px, 2rem, 30px):");
+        // Step 2: change font size
+        let size = prompt("Enter font size (e.g., 20px, 2rem, 30):");
 
         if (size !== null && size.trim() !== "") {
+
+            size = size.trim();
+
+            // If user enters only a number → add 'px'
+            if (!isNaN(size)) {
+                size = size + "px";
+            }
+
             courseLine.style.fontSize = size;
+
         } else {
             alert("No valid font size provided!");
         }
