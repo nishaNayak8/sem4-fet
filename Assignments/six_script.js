140c384ace9d48c7a71aafb8fb7b288f1105941c
nishaNayak8 <4mt24cs115@mite.ac.in>
2026-05-14 17:31:06 +0530

    Update six_script.js

--- Assignments/six_script.js
+++ Assignments/six_script.js
@@ -4,17 +4,17 @@ const courseLine = document.getElementById("myCourse");
 
 courseLine.addEventListener("click", function () {
 
-    // Change text only if something is entered
+    // Change text only if user enters something
     let newText = prompt("Enter new text for the course line:");
 
-    if (newText !== "") {
+    if (newText !== null && newText !== "") {
         courseLine.textContent = newText;
     }
 
-    // Change font size only if something is entered
+    // Change font size only if user enters something
     let size = prompt("Enter font size:");
 
-    if (size !== "") {
+    if (size !== null && size !== "") {
         courseLine.style.fontSize = size;
     }
 
